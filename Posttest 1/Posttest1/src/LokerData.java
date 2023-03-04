f6ce8e0b6499a213c2a0d1bd696a70675ee83c63
Hernirf <92102981+Hernirf@users.noreply.github.com>
2023-03-04 14:27:36 +0800

    Add files via upload

---
+++ Posttest 1/Posttest1/src/LokerData.java
@@ -0,0 +1,15 @@
+public class LokerData {
+    int nomor;
+	String nama;
+	String nama_barang;
+	String nomorHP;
+	String tanggal;
+
+	public LokerData(int nomor_loker, String nama, String nama_barang, String nomorHp, String tanggal){
+		this.nomor = nomor_loker;
+		this.nama = nama;
+		this.nama_barang = nama_barang;
+		this.nomorHP = nomorHp;
+		this.tanggal = tanggal;
+	}
+}
